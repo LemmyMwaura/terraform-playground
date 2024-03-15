f30a685342b987b7bfff2982daf821b7a775c7d2
Lemmy <lemminton@gmail.com>
2024-03-15 17:06:17 +0300

    chore: update and apply gcp config

--- learn-terraform-gcp/main.tf
+++ learn-terraform-gcp/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.51.0"
     }
   }
@@ -10,7 +10,7 @@ terraform {
 provider "google" {
   credentials = file("./service-account/secrets.json")
 
-  project = "<PROJECT_ID>"
+  project = "playground-417313"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
